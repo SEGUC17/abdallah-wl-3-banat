bffe78cd7d69f4376200422321280f5368d0df78
nouraelbanoura <noura.elsaadany@student.guc.edu.eg>
2017-04-06 09:39:54 +0200

    Add files via upload
    akher_update_by_yehia

--- config/passport.js
+++ config/passport.js
@@ -1,23 +1,22 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const Client = require('../controllers/clientsController');
+const User = require('../models/user');
 const config = require('../config/database');
 
 module.exports = function(passport){
-  let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
-  opts.secretOrKey = config.secret;
-  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    Client.getClientById(jwt_payload._doc._id, (err, user) => {
-      if(err){
-        return done(err, false);
-      }
+  let opts = {}
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+    opts.secretOrKey = config.secret;
+    passport.use(new JwtStrategy(opts,function(jwt_payload,done){
+      User.findById(jwt_payload._doc._id,function(err,user){
+        if(err)
+        return done(err,false);
+        if(user)
+        return done(null,user);
+        else
+        return done(null,false);
 
-      if(user){
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    });
-  }));
+      })
+    }))
+
 }
