e241b6ef944a52ca176581848f51e4f8f9588c2e
yahiaosama <yahia.osama8@gmail.com>
2017-04-06 22:17:37 +0200

    final

---
+++ routes/admins.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+const passport=require('passport');
+const jwt=require('jsonwebtoken');
+
+var adminsController = require('../controllers/adminsController');
+
+router.get('/ban/:clientid',passport.authenticate('jwt',{session:false}),
+	adminsController.banClient);
+router.get('/logout', passport.authenticate('jwt', {session:false}),
+ adminsController.logout );
+router.get('/getapps',passport.authenticate('jwt',{session:false}),adminsController.getAdminApplications);
+router.post('/approve/:id/:approval',adminsController.acceptOrDecline);
+router.get('/removebprovider',passport.authenticate('jwt',{session:false}),adminsController.removeProvider);
+
+module.exports = router;
