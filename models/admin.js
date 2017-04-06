5bea015a1c627a2745178024514dcb1f22cb7ebf
nouraelbanoura <noura.elsaadany@student.guc.edu.eg>
2017-04-06 10:49:50 +0200

    Add files via upload

--- models/admin.js
+++ models/admin.js
@@ -1,11 +1,13 @@
 var mongoose = require('mongoose');
+var applicationsSchema = mongoose.Schema({bid:String});
 var adminSchema = mongoose.Schema({
   uid:{
     type:String
 },
 email:{
   type:String
-}
+},
+applications:[applicationsSchema]
 
 })
 
