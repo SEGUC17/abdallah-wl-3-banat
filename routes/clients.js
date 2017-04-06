0d8b25ca1e84864b0e40121a9b4beb982999ce9c
ranahaleem09 <rana.mohamed-mohamed@student.guc.edu.eg>
2017-04-06 22:17:05 +0200

    Update clients.js

--- routes/clients.js
+++ routes/clients.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 var clientsController = require('../controllers/clientsController');
 
-router.get('/ViewReviews/:id',clientsController.viewReviews);
-router.post('/PostReview/:id',clientsController.postReview);
+router.get('/ViewReviews/:id', clientsController.viewReviews);
+router.post('/PostReview/:id', passport.authenticate('jwt',{session:false}),
+clientsController.postReview);
 
 module.exports = router;
